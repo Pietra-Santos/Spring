5089f052831c691c7d5531c1c1e0edb8b6b8ef2b
Pietra-Santos <pietra0608@gmail.com>
2021-03-20 13:40:05 -0300

    Criadas as classes tema, e add relacionamento entre entidades

--- src/main/java/org/generation/blogPedrita/model/Tema.java
+++ src/main/java/org/generation/blogPedrita/model/Tema.java
@@ -11,6 +11,8 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 @Entity
 @Table(name = "tb_tema")
 public class Tema {
@@ -23,6 +25,7 @@ public class Tema {
 	private String descricao;
     
 	@OneToMany(mappedBy = "tema", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("tema")
 	private List<Postagem> postagem;
 
 	public long getId() {
